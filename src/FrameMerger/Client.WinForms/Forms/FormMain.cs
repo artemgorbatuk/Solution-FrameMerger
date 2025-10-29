fb781386358e596703ee1386433144f27c9bb336
Artem Gorbatuk <gorbatuk.am@gmail.com>
2025-10-29 09:25:42 +0300

    Добавить WinForms клиент FrameMerger

---
+++ src/FrameMerger/Client.WinForms/Forms/FormMain.cs
@@ -0,0 +1,15 @@
+namespace Client.WinForms
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
